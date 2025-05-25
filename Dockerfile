8d3e9b8799511d1f76eeaaeaa9a3c5409cfda389
Max Netyaga <max.netyaga@gmail.com>
2025-05-25 21:57:14 +0300

    add matrix endpoint

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,13 @@ RUN apk update && apk add --no-cache \
 # Set working directory
 WORKDIR /app
 
+COPY pyproject.toml uv.lock ./
+RUN uv sync
 
 # Copy the rest of the app
 COPY Makefile .
 COPY spaceship spaceship
 COPY build build
-
-COPY pyproject.toml uv.lock ./
-RUN uv sync
 
 # Expose port used by FastAPI
 EXPOSE 8000
--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,26 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+import numpy as np
 
 router = APIRouter()
 
 
-@router.get('')
+@router.get("")
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    return {"msg": "Hello, World!"}
+
+
+class MatrixRes(BaseModel):
+    matrix_a: list
+    matrix_b: list
+    product: list
+
+
+@router.get("/matrix")
+def matrix() -> MatrixRes:
+    m_a = np.random.rand(10, 10)
+    m_b = np.random.rand(10, 10)
+    product = m_a * m_b
+    return MatrixRes(
+        matrix_a=m_a.tolist(), matrix_b=m_b.tolist(), product=product.tolist()
+    )
